0d098b2e8c87fba537eaaa71eeb3a962dd0b24f0
Кузичев Александр <pulsarmn@yandex.ru>
2025-03-02 15:49:34 +0300

    add: `contacts` and `size` fields to the Storage class

--- src/main/java/com/pulsar/Storage.java
+++ src/main/java/com/pulsar/Storage.java
@@ -1,5 +1,18 @@
 package com.pulsar;
 
 public class Storage {
-    
+
+    private Contact[] contacts;
+    private int size;
+
+    public Storage() {
+        this(100);
+    }
+
+    public Storage(int size) {
+        if (size < 1) {
+            throw new IllegalArgumentException("The size must be greater then 0");
+        }
+        this.contacts = new Contact[size];
+    }
 }
