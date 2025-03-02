f4c0f91c922f922fe3cfe084d44ab1fb14d034eb
Кузичев Александр <pulsarmn@yandex.ru>
2025-03-02 15:46:41 +0300

    add: boiler plate methods to the Contact class

--- src/main/java/com/pulsar/Contact.java
+++ src/main/java/com/pulsar/Contact.java
@@ -1,5 +1,7 @@
 package com.pulsar;
 
+import java.util.Objects;
+
 public class Contact {
 
     private final String name;
@@ -8,5 +10,33 @@ public class Contact {
     public Contact(String name, String phoneNumber) {
         this.name = name;
         this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Contact contact = (Contact) o;
+        return Objects.equals(name, contact.name) && Objects.equals(phoneNumber, contact.phoneNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, phoneNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "name='" + name + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
     }
 }
