10272da859842694fa43c521e7892ae035903e90
Кузичев Александр <pulsarmn@yandex.ru>
2025-03-02 15:45:50 +0300

    add: two fields and constructor to the Contact class

--- src/main/java/com/pulsar/Contact.java
+++ src/main/java/com/pulsar/Contact.java
@@ -1,5 +1,12 @@
 package com.pulsar;
 
 public class Contact {
-    
+
+    private final String name;
+    private final String phoneNumber;
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
 }
