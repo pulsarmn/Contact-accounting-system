3f0a8e737fbe25aa129e58d3960622d693549b91
Кузичев Александр <pulsarmn@yandex.ru>
2025-03-02 16:21:37 +0300

    add: implement the add() and findFreeIndex() method in the Storage class

--- src/main/java/com/pulsar/Storage.java
+++ src/main/java/com/pulsar/Storage.java
@@ -4,6 +4,7 @@ public class Storage {
 
     private Contact[] contacts;
     private int size;
+    private static final int INDEX_OCCUPIED = -1;
 
     public Storage() {
         this(100);
@@ -14,5 +15,34 @@ public class Storage {
             throw new IllegalArgumentException("The size must be greater then 0");
         }
         this.contacts = new Contact[size];
+    }
+
+    public boolean add(String name, String phoneNumber) {
+        if (size == contacts.length) {
+            return false;
+        }
+
+        int index = findFreeIndex();
+        if (index != INDEX_OCCUPIED) {
+            Contact contact = new Contact(name, phoneNumber);
+            contacts[index] = contact;
+            size++;
+            return true;
+        }
+
+        return false;
+    }
+
+    private int findFreeIndex() {
+        int index = INDEX_OCCUPIED;
+
+        for (int i = 0; i < contacts.length; i++) {
+            if (contacts[i] == null) {
+                index = i;
+                break;
+            }
+        }
+
+        return index;
     }
 }
