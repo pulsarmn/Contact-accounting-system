15fb8e2a3dd6042c4689dc1966ae7974de3b4de9
Кузичев Александр <pulsarmn@yandex.ru>
2025-03-03 08:30:37 +0300

    add: implement the println() method in the Printer class

--- src/main/java/com/pulsar/Printer.java
+++ src/main/java/com/pulsar/Printer.java
@@ -36,4 +36,8 @@ public final class Printer {
     public static void print(String message) {
         System.out.print(message);
     }
+
+    public static void println(String message) {
+        System.out.println(message);
+    }
 }
