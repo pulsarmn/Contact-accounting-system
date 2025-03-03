8993fd15083798138858a9a67c76e76528dfa3f7
Кузичев Александр <pulsarmn@yandex.ru>
2025-03-03 09:03:02 +0300

    add: implement the validateName() method in the Validator class

--- src/main/java/com/pulsar/Validator.java
+++ src/main/java/com/pulsar/Validator.java
@@ -2,5 +2,9 @@ package com.pulsar;
 
 public class Validator {
 
-    
+    public void validateName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Некорректное имя пользователя: %s".formatted(name));
+        }
+    }
 }
