f706d79bf6e180f3592f62a81cb5bfa9a51b9b43
Кузичев Александр <pulsarmn@yandex.ru>
2025-03-03 08:04:43 +0300

    add: create the Printer class

---
+++ src/main/java/com/pulsar/Printer.java
@@ -0,0 +1,9 @@
+package com.pulsar;
+
+public final class Printer {
+
+    private static final String BOLD_RED = "\033[1;31m";
+    private static final String RESET = "\033[0m";
+
+    private Printer() {}
+}
