01dfffe786185ee7895e54cd4de4e0e873293cf6
Кузичев Александр <pulsarmn@yandex.ru>
2025-03-03 11:06:30 +0300

    add: implement the main() method in the Main class

--- src/main/java/com/pulsar/Main.java
+++ src/main/java/com/pulsar/Main.java
@@ -3,6 +3,7 @@ package com.pulsar;
 public class Main {
 
     public static void main(String[] args) {
-
+        Application application = new Application();
+        application.start();
     }
 }
