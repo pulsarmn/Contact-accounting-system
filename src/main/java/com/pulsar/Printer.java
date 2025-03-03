863fa3cf3f5f2e66bca78015dcf4fe8247566270
Кузичев Александр <pulsarmn@yandex.ru>
2025-03-03 08:05:10 +0300

    add: implement the mainMenu() method in the Printer class

--- src/main/java/com/pulsar/Printer.java
+++ src/main/java/com/pulsar/Printer.java
@@ -6,4 +6,12 @@ public final class Printer {
     private static final String RESET = "\033[0m";
 
     private Printer() {}
+
+    public static void mainMenu() {
+        System.out.println("1. Добавить контакт");
+        System.out.println("2. Просмотреть контакты");
+        System.out.println("3. Найти контакт");
+        System.out.println("4. Удалить контакт");
+        System.out.println("5. Выйти");
+    }
 }
